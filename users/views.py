2e7b9d40b9d9fc749ae625e066101f7e33da7f0e
Farouk-Ben-Amor <faaroukbnamor@gmail.com>
2023-04-26 17:21:32 +0100

    Styling Login & Register Page

--- users/views.py
+++ users/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render ,redirect
 from django.contrib.auth import login,authenticate,logout
 from django.contrib import messages
+from .forms import CustomUserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required 
 from .models import Profile
 
 
 def loginUser(request):
+    page = 'login'
     if request.user.is_authenticated:
        return redirect('profiles')
        
@@ -28,8 +30,28 @@ def loginUser(request):
 
 def logoutUser(request):
    logout(request)
-   messages.error(request,'User is logged out ')
+   messages.info(request,'User is logged out ')
    return redirect('login')
+
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+    if request.method=='POST':
+       form = CustomUserCreationForm(request.POST)
+       if form.is_valid():
+          user = form.save(commit=False)
+          user.username = user.username.lower()
+          user.save()
+
+          messages.success(request,'User account was created !')
+          login(request,user)
+          return redirect(('profiles'))
+       else:
+          messages.success(request,'An error has occurred during registration!')
+    context = {'page':page,'form':form}
+    return render(request,'users/login_register.html',context)
+
 def profiles(request):
     profiles = Profile.objects.all()
     context = {'profiles' : profiles}
