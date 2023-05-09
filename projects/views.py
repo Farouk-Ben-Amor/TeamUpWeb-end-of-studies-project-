3f30a21772d9cb81078bac3e1d29c220a8696fd4
Farouk-Ben-Amor <faaroukbnamor@gmail.com>
2023-05-09 13:27:15 +0100

    Reviews Done (single-proj/prModels/prViews/forms)

--- projects/views.py
+++ projects/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required 
 from .utils import searchProjects , paginateProjects
+from django.contrib import messages
 from .models import Project ,Tag
-from .forms import ProjectForm 
+from .forms import ProjectForm ,ReviewForm 
 
 def projects(request):
      
@@ -12,10 +13,21 @@ def projects(request):
     context = {'projects' : projects ,'search_query': search_query , 'custom_range' : custom_range }
     return render(request, 'projects/projects.html',context)
 
-def project(request ,pk):
+def project(request , pk):
     projectObj = Project.objects.get(id=pk)
-    print('projectObj : ',projectObj)
-    return render(request, 'projects/single-project.html',{'project': projectObj, })
+    form = ReviewForm()
+    #print('projectObj : ',projectObj)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.project = projectObj
+        review.owner = request.user.profile    
+        review.save()
+        #Update project votecount 
+        projectObj.getVoteCount
+        messages.success(request,'Your review was successufully submitted')
+        return redirect('project' , pk = projectObj.id)
+    return render(request, 'projects/single-project.html',{'project' : projectObj , 'form' : form })
 
 @login_required(login_url='login')
 def createProject(request):
