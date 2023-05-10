270ea59e73cf7dc408114cdc7560884065b7f775
Farouk-Ben-Amor <faaroukbnamor@gmail.com>
2023-05-10 17:58:08 +0100

    sending welcome mails to new users done(with smtp)

--- FaroukPlatform/settings.py
+++ FaroukPlatform/settings.py
@@ -105,7 +105,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+#=> For sending welcome emails:
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'platform0.me@gmail.com' 
+EMAIL_HOST_PASSWORD =  'eteszlotdbsiwtes'
+EMAIL_USE_TLS = True 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
--- users/signals.py
+++ users/signals.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save , post_delete
 from django.dispatch import receiver
 
+from django.core.mail import send_mail
+from django.conf import settings
 
 #=>Here the signals :
 #@receiver(post_save,sender=Profile)
@@ -15,7 +17,17 @@ def createProfile(sender,instance,created,**kwargs):
             username = user.username,
             email = user.email,
             name = user.first_name,
-            )
+        )
+        subject = 'Welcome to our platform'
+        message = "We are happy that you're here !"
+        
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently = False,
+        )
 
 def updateUser(sender,instance,created,**kwargs):
     profile = instance
