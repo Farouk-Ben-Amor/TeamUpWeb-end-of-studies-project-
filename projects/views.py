30bd8ee87855e092ac592bb23d26e20b240bc948
Farouk-Ben-Amor <faaroukbnamor@gmail.com>
2023-05-21 15:05:24 +0100

    some bugs fixes

--- projects/views.py
+++ projects/views.py
@@ -35,12 +35,16 @@ def createProject(request):
     form = ProjectForm()
     
     if request.method=='POST':
+      newtags = request.POST.get("newtags").replace(',', " ").split()
 
       form = ProjectForm(request.POST,request.FILES)
       if form.is_valid():
         project = form.save(commit=False)
         project.owner = profile
         project.save()
+        for tag in newtags:
+           tag, created = Tag.objects.get_or_create(name=tag)
+           project.tags.add(tag)
         return redirect('account')
       
     context = {'form' : form}
@@ -53,10 +57,14 @@ def updateProject(request, pk):
     #=>this eliminated  and replaced with the one above it for privacy issues : project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
     if request.method=='POST':
-
+      newtags = request.POST.get("newtags").replace(',', " ").split()
+      
       form = ProjectForm(request.POST , request.FILES , instance=project)
       if form.is_valid():
-        form.save()
+        project =   form.save()
+        for tag in newtags:
+           tag, created = Tag.objects.get_or_create(name=tag)
+           project.tags.add(tag)
         return redirect('account')
     context = {'form' : form}
     return render(request , 'projects/project_form.html', context)
